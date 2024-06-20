5ab26c28dc5d05693246390e25596d882414483c
Marcoant007 <marcoantnovo@gmail.com>
2024-06-20 18:01:52 -0300

    feat: init project and docker compose

---
+++ src/main/java/org/marcoantdev/app/entity/Poi.java
@@ -0,0 +1,42 @@
+package org.marcoantdev.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Poi {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+    private int coordenadaX;
+    private int coordenadaY;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCoordenadaX() {
+        return coordenadaX;
+    }
+    public void setCoordenadaX(int coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+    public int getCoordenadaY() {
+        return coordenadaY;
+    }
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+}

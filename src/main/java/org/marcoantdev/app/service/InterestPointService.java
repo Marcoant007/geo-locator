4d84e3784b9d1d943fa9ba253e318e21d00610e0
Marcoant007 <marcoantnovo@gmail.com>
2024-06-24 00:35:12 -0300

    chore: remove api exception because exists global exception

--- src/main/java/org/marcoantdev/app/service/InterestPointService.java
+++ src/main/java/org/marcoantdev/app/service/InterestPointService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 import org.marcoantdev.app.dto.InterestPointDTO;
 import org.marcoantdev.app.dto.ProximitySearchDTO;
 import org.marcoantdev.app.entity.InterestPoint;
-import org.marcoantdev.app.exception.ApiException;
 import org.marcoantdev.app.repository.InterestPointRepository;
 import org.marcoantdev.app.utils.CalculateUtil;
 
@@ -25,40 +24,24 @@ public class InterestPointService {
 
     @Transactional
     public InterestPointDTO createInterestPoint(InterestPointDTO receiveDTO) {
-        try {
-            InterestPoint interestPoint = new InterestPoint();
-            interestPoint.setName(receiveDTO.getName());
-            interestPoint.setCoordinateX(receiveDTO.getCoordinateX());
-            interestPoint.setCoordinateY(receiveDTO.getCoordinateY());
-            repository.persist(interestPoint);
-            return InterestPointDTO.returnDTO(interestPoint);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ApiException("Ops... Ocorreu um erro ao cadastrar o ponto futuro", 500);
-        }
+        InterestPoint interestPoint = new InterestPoint();
+        interestPoint.setName(receiveDTO.getName());
+        interestPoint.setCoordinateX(receiveDTO.getCoordinateX());
+        interestPoint.setCoordinateY(receiveDTO.getCoordinateY());
+        repository.persist(interestPoint);
+        return InterestPointDTO.returnDTO(interestPoint);
     }
 
     public List<InterestPointDTO> getAllInterestPoints() {
-        try {
-            List<InterestPoint> interestPoints = repository.listAll();
-            return interestPoints.stream().map(InterestPointDTO::returnDTO).collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ApiException("Ops... Ocorreu um erro ao listar os pontos futuros", 500);
-        }
+        List<InterestPoint> interestPoints = repository.listAll();
+        return interestPoints.stream().map(InterestPointDTO::returnDTO).collect(Collectors.toList());
     }
 
     public List<InterestPointDTO> getAllInterestPointsProximity(ProximitySearchDTO searchDTO) {
-        try {
-            List<InterestPoint> allPoints = repository.listAll();
-            return allPoints.stream()
-                    .filter(point -> calculateUtil.calculateDistance(point.getCoordinateX(), point.getCoordinateY(),
-                            searchDTO.getX(), searchDTO.getY()) <= searchDTO.getdMax())
-                    .map(InterestPointDTO::returnDTO).collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ApiException("Ops... Ocorreu um erro ao buscar os pontos por proximidade", 500);
-        }
+        List<InterestPoint> allPoints = repository.listAll();
+        return allPoints.stream()
+                .filter(point -> calculateUtil.calculateDistance(point.getCoordinateX(), point.getCoordinateY(),
+                        searchDTO.getX(), searchDTO.getY()) <= searchDTO.getdMax())
+                .map(InterestPointDTO::returnDTO).collect(Collectors.toList());
     }
 }
-
